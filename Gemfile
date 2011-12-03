4441e107caf3e875b1914cc2edaf4ebec653593b
bmaher <briancmaher@gmail.com>
2011-12-03 17:15:09 +0000

    Added RVM support to Capistrano.

--- Gemfile
+++ Gemfile
@@ -6,10 +6,9 @@ gem 'sqlite3'
 
 group :assets do
   gem 'sass-rails',   '~> 3.1.5'
-  gem 'coffee-rails', '~> 3.1.1'
-  gem 'uglifier', '>= 1.0.3'
-  gem 'haml'
 end
+
+gem 'haml'
 
 gem 'jquery-rails'
 
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,27 +1,30 @@
 set :application, "subversion-stats"
-
+set :domain,      "dojann.co.uk"
 set :repository,  "git@github.com:bmaher/subversion-stats.git"
-set :scm, :git
+set :use_sudo,    false
+set :deploy_to,   "/var/www/#{application}"
+set :scm,         "git"
+set :user,        "deploy"
 
-set :user, "deploy"
-set :use_sudo, false
-set :deploy_to, "/var/www/#{application}"
-set :deploy_via, :remote_cache
+role :app, domain
+role :web, domain
+role :db,  domain, :primary => true
 
-role :web, "dojann.co.uk"
-role :app, "dojann.co.uk"
-role :db,  "dojann.co.uk", :primary => true
-
-after "deploy", "deploy:bundle_gems"
-after "deploy:bundle_gems", "deploy:restart"
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+require "rvm/capistrano"
+set :rvm_ruby_string, "1.9.3-p0"
 
 namespace :deploy do
-  task :bundle_gems do
-    run "cd #{deploy_to}/current && bundle --deployment"
+  task :start, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
   end
-  task :start do ; end
-  task :stop do ; end
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "touch #{File.join(current_path,'tmp','restart.txt')}"
+
+  task :stop, :roles => :app do
+    # Do nothing.
+  end
+
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
   end
 end
