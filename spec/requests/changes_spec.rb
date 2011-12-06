cb0d53db33161023a31670ef3cacd4fe5c8bd9e2
bmaher <briancmaher@gmail.com>
2011-12-06 16:08:20 +0000

    Added requests test for Changes Controller.

---
+++ spec/requests/changes_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe "Changes" do
+
+  describe "create change" do
+    
+    describe "failure" do
+      it "should not make a new change" do
+        lambda do
+          visit new_change_path
+          fill_in "Revision", :with => ""
+          fill_in "Commit", :with => ""
+          fill_in "Status", :with => ""
+          fill_in "Project Root", :with => ""
+          fill_in "Filepath", :with => ""
+          fill_in "Fullpath", :with => ""
+          click_button
+          response.should render_template("changes/new")
+          response.should have_selector("div#error_explanation")
+        end.should_not change(Change, :count)
+      end
+    end
+    
+    describe "success" do
+      it "should make a new change" do
+        lambda do
+          visit new_change_path
+          fill_in "Revision", :with => 1
+          fill_in "Commit", :with => 1
+          fill_in "Status", :with => "A"
+          fill_in "Project Root", :with => "/target"
+          fill_in "Filepath", :with => "/target/src/main"
+          fill_in "Fullpath", :with => "/target/src/main/file.txt"
+          click_button
+          response.should have_selector("p", :content => "Change was successfully created.")
+          response.should render_template("changes/show")
+        end.should change(Change, :count).by(1)
+      end
+    end 
+  end
+end
