ee51419251ba3680fd43fc439bfdea1fd5863089
bmaher <briancmaher@gmail.com>
2011-12-06 18:36:41 +0000

    Created simple XML Parser for sample SVN data.

---
+++ script/xml_processor.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'xml'
+
+file = File.open("./sample.xml")
+raw_xml = file.read
+
+source = XML::Parser.string(raw_xml)
+content = source.parse
+
+entries = content.root.find('./logentry')
+entries.each do |entry|
+  revision = entry['revision']
+  puts revision
+  
+  author = entry.find_first('author').content
+  puts author
+  
+  date = entry.find_first('date').content
+  puts date
+  
+  paths = entry.find('./paths/path')
+  paths.each do |path|
+    action = path['action']
+    puts action
+    
+    full_path = path.content
+    puts full_path
+    
+    file_paths = full_path.partition(/(branch|tag|trunk)/i)
+   
+    project_root = file_paths[0]
+    puts project_root
+    
+    file_path = file_paths[2]
+    puts file_path
+  end
+  
+  message = entry.find_first('msg')
+  puts message
+  
+  puts '------------------------------------------'
+end
