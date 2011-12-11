893ff958928656363402c911f9b8c1312abf9d6c
bmaher <briancmaher@gmail.com>
2011-12-11 12:34:24 +0000

    Added symlink for private files.

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,6 +32,8 @@ namespace :deploy do
   
   task :symlink_shared do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/config/newrelic.yml #{release_path}/config/newrelic.yml"
+    run "ln -nfs #{shared_path}/config/secret_token.rb #{release_path}/config/initializers/secret_token.rb"
   end
 end
 
