6af7579c484dd51ebe59f7abb23cdf0a30329eb2
Brian Maher <briancmaher@gmail.com>
2012-09-07 19:52:45 +0200

    #398713: Created basic version of commits by year stats.

--- app/helpers/stats_helper.rb
+++ app/helpers/stats_helper.rb
@@ -3,4 +3,8 @@ module StatsHelper
   def total_commits_for(user)
     Commit.find_all_by_user_id(user).count
   end
+  
+  def commits_by_year
+    Commit.count(:all, :group => "Year(datetime)")
+  end
 end
