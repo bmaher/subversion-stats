ce5209518c7d7491266e3f4091d5c14f8db9999d
Brian Maher <briancmaher@gmail.com>
2012-09-09 23:45:04 +0200

    Updated commits by month to account for projects.

--- app/helpers/stats_helper.rb
+++ app/helpers/stats_helper.rb
@@ -28,8 +28,12 @@ module StatsHelper
     Commit.count(:conditions => "user_id = #{user.id}", :group => "Year(datetime)")
   end
   
-  def commits_by_month
-    Commit.count(:all, :group => "Month(datetime)")
+  def commits_by_month_for_project(project)
+    months = Array.new
+    users_for_project(project).each do |user|
+      months << commits_by_month_for(user)
+    end
+    months.inject{ |months, count| months.merge(count){ |key, old_value, new_value| old_value + new_value } }.to_a
   end
   
   def commits_by_month_for(user)
