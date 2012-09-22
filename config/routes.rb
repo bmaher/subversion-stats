f8e3292602507dd71e39e01c867128fc916e5cb3
Brian Maher <briancmaher@gmail.com>
2012-09-22 16:02:27 +0200

    #411570: Adding authorization check for /sidekiq page.

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ SubversionStats::Application.routes.draw do
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
   root :to => "home#index"
   resources :projects, :committers, :commits, :changes, :stats, :uploads
-  mount Sidekiq::Web, at: '/sidekiq'
+  constraint = lambda { |request| request.env["warden"].authenticate? and request.env['warden'].user.has_role? :admin }
+  constraints constraint do
+    mount Sidekiq::Web => '/sidekiq'
+  end
 end
